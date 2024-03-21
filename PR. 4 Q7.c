4d140df4e78821d0692bcd489318737423c53c5e
Vivekprajapati04 <161919206+Vivekprajapati04@users.noreply.github.com>
2024-03-21 18:05:22 +0530

    Add files via upload

---
+++ PR. 4 Q7.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+main()
+{
+    int i, j;
+    for (i = 0; i <5; i++) 
+	{
+        for (j = 0; j < 5; j++) 
+		{
+            if (i == 0 || i == 5 - 1 || j == 0 || j == 5- 1)
+                printf("*  ");
+            else
+                printf("   ");
+        }
+        printf("\n");
+    }
+}
+
+ 
